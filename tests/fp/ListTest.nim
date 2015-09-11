ca6e96708ad783900c031d0a529b2def228d7022
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2015-09-11 16:50:27 +0600

    Compilation error: List.dup cause stack overflow

--- tests/fp/ListTest.nim
+++ tests/fp/ListTest.nim
@@ -4,16 +4,17 @@ import ../../src/fp/List, unittest, future
 
 suite "List ADT":
 
-  test "Initialization and conversion":
+  test "Initialization, conversion and information":
     let lst = [1, 2, 3, 4, 5].asList
 
     check: lst.head == 1
     check: lst.tail.asSeq == @[2, 3, 4, 5]
     check: $lst == "List(1, 2, 3, 4, 5)"
     check: 1^^2^^3^^4^^5^^Nil[int]() == lst
+    check: lst.length == 5
 
   test "Fold operations":
-    let lst = lc[x|(x <- 1..4),int].asList
+    let lst = lc[x | (x <- 1..4), int].asList
 
     check: lst.foldLeft(0, (x, y) => x + y) == 10
     check: lst.foldLeft(1, (x, y) => x * y) == 24
@@ -22,8 +23,15 @@ suite "List ADT":
     check: lst.foldRight(1, (x, y) => x * y) == 24
 
   test "Drop operations":
-    let lst = lc[x|(x <- 1..100),int].asList
+    let lst = lc[x | (x <- 1..100), int].asList
 
     check: lst.drop(99) == [100].asList
     check: lst.dropWhile(x => x < 100) == [100].asList
+
+  test "Misc functions":
+    let lst = lc[$x | (x <- 'a'..'z'), string].asList
+
+    let lstCopy = lst.dup
+    check: lstCopy == lst
+    check: lstCopy.addr != lst.addr
 
