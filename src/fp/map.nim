1b80f0607f283d6e9163f1a7a71d55271efb9880
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2016-03-29 12:24:27 +0600

    Added getters, setters and map functions to the Map ADT

--- src/fp/map.nim
+++ src/fp/map.nim
@@ -17,11 +17,30 @@ proc value*[K,V](item: MapItem[K,V]): V = item[1]
 proc `$`[K,V](item: MapItem[K,V]): string = $item.key & " => " & $item.value
 
 proc `$`*[K,V](m: Map[K,V]): string =
-  "Map(" & List[MapItem[K,V]](m).foldLeft("", (s: string, v: (K,V)) => s & (if s == "": "" else: ", ") & $v) & ")"
+  "Map(" & m.asList.foldLeft("", (s: string, v: (K,V)) => s & (if s == "": "" else: ", ") & $v) & ")"
 
 # future.`->` doesn't support ``MapItem[K,V] -> bool`` declaration
 proc find*[K,V](m: Map[K,V], p: proc(i: MapItem[K,V]): bool): Option[MapItem[K,V]] =
   m.asList.find(p)
 
+proc filter*[K,V](m: Map[K,V], p: proc(i: MapItem[K,V]): bool): Map[K,V] =
+  m.asList.filter(p).asMap
+
 proc get*[K,V](m: Map[K,V], k: K): Option[V] =
   m.find((i: MapItem[K,V]) => i.key == k).map((v: MapItem[K,V]) => v.value)
+
+proc remove*[K,V](m: Map[K,V], k: K): Map[K,V] =
+  m.filter((i: MapItem[K,V]) => i.key != k)
+
+proc `-`*[K,V](m: Map[K,V], k: K): Map[K,V] = m.remove(k)
+
+proc add*[K,V](m: Map[K,V], item: MapItem[K,V]): Map[K,V] =
+  (item ^^ m.asList.filter((i: MapItem[K,V]) => i.key != item.key)).asMap
+
+proc `+`*[K,V](m: Map[K,V], item: MapItem[K,V]): Map[K,V] = m.add(item)
+
+proc map*[K,V,K1,V1](m: Map[K,V], f: proc(item: MapItem[K,V]): MapItem[K1,V1]): Map[K1,V1] =
+  m.asList.map(f).asMap
+
+proc `==`*[K,V](m1, m2: Map[K,V]): bool =
+  m1.asList.forAll((v: MapItem[K,V]) => m1.get(v.key) == m2.get(v.key))
--- tests/fp/map_test.nim
+++ tests/fp/map_test.nim
@@ -1,12 +1,18 @@
-import ../../src/fp/option, ../../src/fp/map, ../../src/fp/list, unittest, future
+import ../../src/fp/option, ../../src/fp/map, ../../src/fp/list, unittest, future, strutils
 
 suite "Map ADT":
+  let m = [(1, "1"), (2, "2"), (3, "3")].asMap
+
   test "Initialization":
-    let m = [(1, "1"), (2, "2"), (3, "3")].asMap
-    let l = [(1, "1"), (2, "2"), (3, "3")].asList
-    echo m
-    echo l
     check: $m == "Map(1 => 1, 2 => 2, 3 => 3)"
     check: m.get(1) == "1".some
     check: m.get(2) == "2".some
     check: m.get(3) == "3".some
+    check: m.get(4) == "4".none
+
+  test "Transformations":
+    check: (m + (4, "4")).get(4) == "4".some
+    check: (m + (1, "11")).get(1) == "11".some
+    check: (m - 1).get(1) == "11".none
+
+    check: m.map((i: MapItem[int, string]) => ($i.key, i.value.parseInt)) == [("3", 3), ("1", 1), ("2", 2)].asMap
