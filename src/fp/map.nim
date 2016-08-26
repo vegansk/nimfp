6c9186f1eaa169cc3280c9921f813350188282d3
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2016-08-26 17:48:41 +0700

    Implement Map using boost.data.rbtree

--- src/fp/map.nim
+++ src/fp/map.nim
@@ -1,49 +1,74 @@
-import list, future, option, boost.data.rbtree
+import list, future, option, boost.data.rbtree, sequtils
 
 type
-  Map*[K,V] = distinct List[tuple[k: K, v: V]]
+  Map*[K,V] = RBTree[K,V]
 
-proc asMap*[K,V](lst: List[tuple[k: K, v: V]]): Map[K,V] = Map[K,V](lst)
+proc newMap*[K,V]: Map[K,V] = newRBTree[K,V]()
+
+proc asMap*[K,V](lst: List[tuple[k: K, v: V]]): Map[K,V] =
+  lst.foldLeft(newMap[K,V](),  (res: Map[K,V], v: tuple[k: K, v: V]) => res.add(v[0], v[1]))
 proc asMap*[K,V](xs: varargs[tuple[k: K, v: V]]): Map[K,V] = xs.asList.asMap
 
-proc asList*[K,V](m: Map[K,V]): List[tuple[k: K, v: V]] = List[tuple[k: K, v: V]](m)
+proc asList*[K,V](m: Map[K,V]): List[tuple[k: K, v: V]] = toSeq(m.pairs).asList.map((t: (K,V)) => (k: t[0], v: t[1]))
 
 proc key*[K,V](item: tuple[k: K, v: V]): K = item[0]
 proc value*[K,V](item: tuple[k: K, v: V]): V = item[1]
-proc asStr[K,V](item: tuple[k: K, v: V]): string = $item.key & " => " & $item.value
 
 proc `$`*[K,V](m: Map[K,V]): string =
-  "Map(" & m.asList.foldLeft("", (s: string, v: (K,V)) => s & (if s == "": "" else: ", ") & v.asStr) & ")"
+  result = "Map("
+  var first = true
+  for k, v in m.pairs:
+    if not first:
+      result &= ", "
+    else:
+      first = not first
+    result &= $k & " => " & $v
+  result &= ")"
 
 # future.`->` doesn't support ``MapItem[K,V] -> bool`` declaration
 proc find*[K,V](m: Map[K,V], p: proc(i: (K,V)): bool): Option[tuple[k: K, v: V]] =
-  m.asList.find(p)
+  for k, v in m.pairs:
+    if p((k,v)):
+      return (k: k, v: v).some
+  return none(tuple[k: K, v: V])
 
 proc filter*[K,V](m: Map[K,V], p: proc(i: (K,V)): bool): Map[K,V] =
-  m.asList.filter(p).asMap
+  result = newMap[K,V]()
+  for k, v in m.pairs:
+    if p((k, v)):
+      result = result.add(k, v)
 
 proc get*[K,V](m: Map[K,V], k: K): Option[V] =
-  m.find((i: (K,V)) => i.key == k).map((v: (K,V)) => v.value)
+  var v: V
+  if m.maybeGet(k, v):
+    some(v)
+  else:
+    none(V)
 
 proc remove*[K,V](m: Map[K,V], k: K): Map[K,V] =
-  m.filter((i: (K,V)) => i.key != k)
+  m.del(k)
 
-proc `-`*[K,V](m: Map[K,V], k: K): Map[K,V] = m.remove(k)
+template `-`*[K,V](m: Map[K,V], k: K): Map[K,V] = m.remove(k)
 
 proc add*[K,V](m: Map[K,V], item: (K,V)): Map[K,V] =
-  (item ^^ m.asList.filter((i: (K,V)) => i.key != item.key)).asMap
+  m.add(item[0], item[1])
 
-proc `+`*[K,V](m: Map[K,V], item: (K,V)): Map[K,V] = m.add(item)
+template `+`*[K,V](m: Map[K,V], item: (K,V)): Map[K,V] = m.add(item)
 
 proc map*[K,V,K1,V1](m: Map[K,V], f: proc(item: (K,V)): (K1,V1)): Map[K1,V1] =
-  m.asList.map(f).asMap
+  result = newMap[K1,V1]()
+  for k, v in m.pairs:
+    result = result.add(f((k, v)))
 
 proc `==`*[K,V](m1, m2: Map[K,V]): bool =
-  m1.asList.forAll((v: (K,V)) => m1.get(v.key) == m2.get(v.key)) and
-    m2.asList.forAll((v: (K, V)) => m1.get(v.key).isDefined)
+  m1.equals(m2)
 
 proc forEach*[K,V](m: Map[K,V], f: proc(item: (K,V)): void): void =
-  m.asList.forEach(f)
+  for k, v in m.pairs:
+    f((k, v))
 
 proc forAll*[K,V](m: Map[K,V], p: proc(item: (K,V)): bool): bool =
-  m.asList.forAll(p)
+  result = true
+  for k, v in m.pairs:
+    if not p((k, v)):
+      return false
--- tests/fp/test_map.nim
+++ tests/fp/test_map.nim
@@ -4,6 +4,7 @@ suite "Map ADT":
   let m = [(1, "1"), (2, "2"), (3, "3")].asMap
 
   test "Initialization":
+    check: m.asList == [(1, "1"), (2, "2"), (3, "3")].asList
     check: $m == "Map(1 => 1, 2 => 2, 3 => 3)"
     check: m.get(1) == "1".some
     check: m.get(2) == "2".some
@@ -16,12 +17,19 @@ suite "Map ADT":
     check: (m - 1).get(1) == "11".none
 
     let r = m.map((i: (int,string)) => ($i.key, i.value.parseInt))
+    echo r
+    echo([("3", 3), ("1", 1), ("2", 2)].asMap)
     check: r == [("3", 3), ("1", 1), ("2", 2)].asMap
 
   test "Misc":
     var x = 0
     [(1, 100), (2, 200)].asMap.forEach((v: (int, int)) => (x += (x + v.key) * v.value))
     check: x == 20500
+    check: map.find(m, v => (v[0] mod 2 == 0)) == (2, "2").some
+    check: m.filter(v => (v[0] mod 2 == 0)) == [(2, "2")].asMap
+    check: m.remove(1).get(1) == string.none
+    check: m.map(i => (i[0] * 2, i[1] & i[1])) == [(2, "11"), (4, "22"), (6, "33")].asMap
+    check: m.forAll(i => $i[0] == i[1])
 
   test "Equality":
     check: asMap((1, 2), (3, 4)) == asMap((3, 4), (1, 2))
